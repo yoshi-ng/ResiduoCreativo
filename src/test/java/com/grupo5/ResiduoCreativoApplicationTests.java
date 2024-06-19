6f40314259ede67ae87b2947a0bd37592fa7b080
ValitaMardones <valentinamardonesaravena@gmail.com>
2024-06-19 14:37:25 -0400

    Carpeta base del proyecto

---
+++ src/test/java/com/grupo5/ResiduoCreativoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grupo5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResiduoCreativoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
